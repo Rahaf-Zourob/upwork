52570f72fef1735846511a457d9e50b203fbb64e
RahafZourob <zourobrahaf@gmail.com>
2023-10-05 23:55:36 +0300

    add remaining

--- src/pages/FilterPage/index.jsx
+++ src/pages/FilterPage/index.jsx
@@ -3,13 +3,12 @@ import { getMatchJobs } from '@/redux/slices/search'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-export default function FilterPage({params}) {
-  const {isLoading,matchJobs}=useSelector(state=>state.search)
+export default function FilterPage({ params }) {
+  const { isLoading, matchJobs } = useSelector(state => state.search)
   const dispatch = useDispatch()
-  console.log(params.search)
-  useEffect(()=>{
-    getMatchJobs(params.search)
-  },[])
+  useEffect(() => {
+    dispatch(getMatchJobs(params.search))
+  }, [params.search])
   return (
     <MainLayout>
 
